b951b11d6934adb854d70984f20f0aee05d6bdd5
Daigo Shitara <daigo@marcelle.cc>
2018-10-13 22:37:29 +0900

    :construction: action construction

--- src/modules/notification.js
+++ src/modules/notification.js
@@ -1,2 +1,11 @@
 // action types
 export const SET_NOTIFICATION = 'SET_NOTIFICATION'
+
+// action creators
+export function setNotification({ message, feature }) {
+  return {
+    type: `${feature} ${SET_NOTIFICATION}`,
+    payload: message,
+    meta: { feature },
+  }
+}
